60c7f256d075ba3283a8ddd06cac6a42fc60e96b
abj <abj1.rts@gmail.com>
2021-01-22 00:54:48 +0530

    CV update 4

--- src/components/Center.js
+++ src/components/Center.js
@@ -28,60 +28,105 @@ function Center(props) {
     )
 
     return (
-        <Container fluid
-            style={styles.container}
-            className="smooth-scroll"
-            id="home"
-        >
-            <Row className="pt-2 pb-0 py-sm-0" style={styles.row} id="" >
-                <Col className="py-auto py-lg-5 d-flex justify-content-center" style={styles.col}>
-                    <Card className="mx-auto my-5 border-0" style={styles.card}>
-                        <Card.Title className="mx-auto mb-0" style={styles.title}>
-                            HI <span style={{ color: "#FD5F00", fontFamily: "product sans", fontWeight: "bolder" }}>,</span> I AM<br />
-                                ABHIJEET<br />
-                                KUMAR<span style={{ color: "#FD5F00", fontFamily: "Product Sans", fontWeight: "bolder" }}>.</span>
-
-                        </Card.Title>
-                        <Card.Text className="my-3" style={styles.text}>
-                            <div>
-                                F.P ENGINEERING STUDENT 
-                                {/* {'&'}<br /> */}
-                                {/* FULL-STACK DEVELOPER (Web {'&'} Apps) */}
-                            </div>
-                            <Card.Link target="_blank" href="https://drive.google.com/file/d/1vl_USL1y0HtcktoBRt2yWjVrfoupBxei/view?usp=drivesdk">
-                                <button className="mt-3 btn" type="button"
-                                    style={{ backgroundColor: "#FD5F00", color: "#fff", height: "3em", width: "10.5em" }}
-                                >
-                                    Download Rèsumè
-                                </button>
-                            </Card.Link>
-                        </Card.Text>
-                        <div styles={styles.link} className="mx-auto mt-1">
-                            <Card.Link href="https://www.linkedin.com/in/abhijeet-kumar-208478163/"
-                                target="_blank"
-                                style={styles.link} >
-                                <img src={In} alt="IN" />
-                            </Card.Link>
-                            <Card.Link href="https://github.com/Abhijeet-14"
-                                target="_blank"
-                                style={styles.link} >
-                                <img src={Gh} alt="GH" style={{ height: "2em", width: "2em" }} />
-                            </Card.Link>
-                            <Card.Link href="https://www.youtube.com/channel/UCyhChkyobpFvtqQATIMM9xQ?view_as=subscriber"
-                                target="_blank"
-                                style={styles.link} >
-                                <img src={Yt} alt="YT" />
-                            </Card.Link>
-                        </div>
-                    </Card>
-                </Col>
-            </Row>
-            <About proImg={open.proImg} />
-            {open.projects === 1 && <Projects />}
-            {/* <Projects /> */}
-            {open.software === 0 && <Software />}
-        </Container >
-    )
+      <Container
+        fluid
+        style={styles.container}
+        className="smooth-scroll"
+        id="home"
+      >
+        <Row className="pt-2 pb-0 py-sm-0" style={styles.row} id="">
+          <Col
+            className="py-auto py-lg-5 d-flex justify-content-center"
+            style={styles.col}
+          >
+            <Card className="mx-auto my-5 border-0" style={styles.card}>
+              <Card.Title className="mx-auto mb-0" style={styles.title}>
+                HI{" "}
+                <span
+                  style={{
+                    color: "#FD5F00",
+                    fontFamily: "product sans",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  ,
+                </span>{" "}
+                I AM
+                <br />
+                ABHIJEET
+                <br />
+                KUMAR
+                <span
+                  style={{
+                    color: "#FD5F00",
+                    fontFamily: "Product Sans",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  .
+                </span>
+              </Card.Title>
+              <Card.Text className="my-3" style={styles.text}>
+                <div>
+                  F.P ENGINEERING STUDENT
+                  {/* {'&'}<br /> */}
+                  {/* FULL-STACK DEVELOPER (Web {'&'} Apps) */}
+                </div>
+                <Card.Link
+                  target="_blank"
+                  href="https://drive.google.com/file/d/18aTckzBBur8VK80lySEv-gWLOM6PwGLg/view?usp=drivesdk"
+                  // href="https://drive.google.com/file/d/1vl_USL1y0HtcktoBRt2yWjVrfoupBxei/view?usp=drivesdk"
+                >
+                  <button
+                    className="mt-3 btn"
+                    type="button"
+                    style={{
+                      backgroundColor: "#FD5F00",
+                      color: "#fff",
+                      height: "3em",
+                      width: "10.5em",
+                    }}
+                  >
+                    Download Rèsumè
+                  </button>
+                </Card.Link>
+              </Card.Text>
+              <div styles={styles.link} className="mx-auto mt-1">
+                <Card.Link
+                  href="https://www.linkedin.com/in/abhijeet-kumar-208478163/"
+                  target="_blank"
+                  style={styles.link}
+                >
+                  <img src={In} alt="IN" />
+                </Card.Link>
+                <Card.Link
+                  href="https://github.com/Abhijeet-14"
+                  target="_blank"
+                  style={styles.link}
+                >
+                  <img
+                    src={Gh}
+                    alt="GH"
+                    style={{ height: "2em", width: "2em" }}
+                  />
+                </Card.Link>
+                <Card.Link
+                  href="https://www.youtube.com/channel/UCyhChkyobpFvtqQATIMM9xQ?view_as=subscriber"
+                  target="_blank"
+                  style={styles.link}
+                >
+                  <img src={Yt} alt="YT" />
+                </Card.Link>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+        <About proImg={open.proImg} />
+        {open.projects === 1 && <Projects />}
+        {/* <Projects /> */}
+        {open.software === 0 && <Software />}
+      </Container>
+    );
 }
 
 
