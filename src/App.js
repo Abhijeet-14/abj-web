8313b712440f35df1ff310d9534c84509dedfd7b
Kumar, Abhijeet <abhijeetkumar4@deloitte.com>
2021-07-28 02:12:58 +0530

    Changes: App.js, checking process.env

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Main from './MainCompo'
+import Main from "./MainCompo";
 // import './App.css';
 
 function App() {
+  console.log(process.env);
   return (
-    <div style={{  margin: 0, padding: 0}}>
+    <div style={{ margin: 0, padding: 0 }}>
       <Main />
     </div>
   );
 }
 
 export default App;
-
-
-
