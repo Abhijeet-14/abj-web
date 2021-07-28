18eb77fb049726132dc92c7f8c7826801aeed82a
Kumar, Abhijeet <abhijeetkumar4@deloitte.com>
2021-07-28 17:55:35 +0530

    refactoring App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Suspense } from "react";
 // import './App.css';
 
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { Tooltip } from "@material-ui/core";
 
 const LazyMain = React.lazy(() => import("./MainCompo"));
 
@@ -12,9 +13,11 @@ const Visitors = ({ visitors }) => (
     style={{
       postion: "relative",
     }}
-    title="No. of Visitors"
+    // title="No. of Visitors"
   >
-    <b style={styles.visitors}>{visitors}</b>
+    <Tooltip title="Visitors Count">
+      <b style={styles.visitors}>{visitors}</b>
+    </Tooltip>
   </button>
 );
 
@@ -22,18 +25,37 @@ function App() {
   const [visitors, setVisitors] = React.useState(0);
 
   React.useEffect(() => {
-    (async () => {
-      const res = await fetch(
-        "https://spreadsheets.google.com/feeds/worksheets/1JQ0v-Mo3HRWKbnwV4tOxCCDOEwWjLW7nDyhmbKACBoE/public/basic?alt=json",
-        {
-          crossOrigin: true,
-        }
-      );
-      const data = await res.json();
-      console.log(data?.feed?.entry[1]?.content?.$t);
-      setVisitors(data?.feed?.entry[1]?.content?.$t);
-    })();
+    {
+      process.env.NODE_ENV === "production" &&
+        (async () => {
+          const res = await fetch(
+            "https://sheet.best/api/sheets/4ca4b144-2312-45fc-bc3a-451e03decd3a/0",
+            {
+              crossOrigin: true,
+            }
+          );
+          const data = await res.json();
+
+          setVisitors(data[0]["No of visits"]);
+
+          await handleMethod(data[0]["No of visits"]);
+        })();
+    }
   }, []);
+
+  const handleMethod = async (visits) => {
+    const res = await fetch(
+      "https://sheet.best/api/sheets/4ca4b144-2312-45fc-bc3a-451e03decd3a/0",
+      {
+        method: "PATCH",
+        crossOrigin: true,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "No of visits": Number(visits) + 1 }),
+      }
+    );
+    const data = await res.json();
+    // console.log(data);
+  };
 
   return (
     <div style={{ margin: 0, padding: 0 }}>
@@ -61,12 +83,12 @@ const styles = {
     marginTop: "46vh",
   },
   visitors: {
-    borderRadius: "1rem",
+    borderRadius: "5rem",
     color: "white",
     backgroundColor: "#FD5F00",
     position: "fixed",
     boxShadow: "0 0 .2rem .2rem rgb(10, 10, 255, .2)",
-    padding: "0.2rem .5rem",
+    padding: "0.2rem .4rem",
     right: "3rem",
     bottom: "2rem",
   },
